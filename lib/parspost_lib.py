bd0884e36c4835af7bb06c7ba5a5038dedac1b96
Denis <38821830+denisssss43@users.noreply.github.com>
2020-01-29 20:22:42 +0700

    Удалены неиспользуемые библиотеки

--- lib/parspost_lib.py
+++ lib/parspost_lib.py
@@ -5,9 +5,7 @@ from bs4 import BeautifulSoup
 import re 
 import uuid
 from datetime import datetime, timedelta
-import pymysql
 from contextlib import closing
-from pymysql.cursors import DictCursor
 import ast 
 import copy 
 
@@ -56,21 +54,20 @@ def AddressYandex(string, country='Россия', city='Красноярск'):
 
 
 if __name__ == "__main__":
-	while(True):
-		try:
-			print (datetime.datetime.now(), 'Connect')
-			db.Connect()
-
-			for i in range(3):
-				db.SearchFromGroupWithoutAddress(g_id='123114913', g_title='', offset=i*10, country='Россия', city='Красноярск')
-				db.SearchFromGroupWithoutAddress(g_id='105543780', g_title='', offset=i*10, country='Россия', city='Красноярск')
-				db.SearchFromGroupWithoutAddress(g_id='', g_title='arendav24', offset=i*10, country='Россия', city='Красноярск')
-				db.SearchFromGroupWithoutAddress(g_id='', g_title='arenda.v.krasnoyarske', offset=i*10, country='Россия', city='Красноярск')
-				db.SearchFromGroupWithoutAddress(g_id='76867861', g_title='', offset=i*10, country='Россия', city='Красноярск') 
-				db.SearchFromGroupWithoutAddress(g_id='80318218', g_title='', offset=i*10, country='Россия', city='Красноярск')
-
-			db.CloseConnect()
-			print ('	CloseConnect')
-		except: pass
-    
-    pass
+	print(AddressYandex("", country='Россия', city='Красноярск'))
+#	while(True):
+#		try:
+#			print (datetime.datetime.now(), 'Connect')
+#			db.Connect()
+#
+#			for i in range(3):
+#				db.SearchFromGroupWithoutAddress(g_id='123114913', g_title='', offset=i*10, country='Россия', city='Красноярск')
+#				db.SearchFromGroupWithoutAddress(g_id='105543780', g_title='', offset=i*10, country='Россия', city='Красноярск')
+#				db.SearchFromGroupWithoutAddress(g_id='', g_title='arendav24', offset=i*10, country='Россия', city='Красноярск')
+#				db.SearchFromGroupWithoutAddress(g_id='', g_title='arenda.v.krasnoyarske', offset=i*10, country='Россия', city='Красноярск')
+#				db.SearchFromGroupWithoutAddress(g_id='76867861', g_title='', offset=i*10, country='Россия', city='Красноярск') 
+#				db.SearchFromGroupWithoutAddress(g_id='80318218', g_title='', offset=i*10, country='Россия', city='Красноярск')
+#
+#			db.CloseConnect()
+#			print ('	CloseConnect')
+#		except: pass
