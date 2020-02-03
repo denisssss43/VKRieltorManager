caabddf26979877821cdc959c5c9191bfcb2e2a5
Денис Карасев <karasev.bpi1701@gmail.com>
2020-02-03 18:00:25 +0700

    изменения в либо бд

--- lib/db_lib.py
+++ lib/db_lib.py
@@ -3,11 +3,20 @@
 def GetGroups():
 	"""Получение списка групп, по которым необходимо собирать информацию"""
 	result = list()
-    
+	
 	result.append({'country':'Россия', 'city':'Красноярск', 'id_group':'public9751268'})
 	result.append({'country':'Россия', 'city':'Красноярск', 'id_group':'public123114913'})
 	result.append({'country':'Россия', 'city':'Красноярск', 'id_group':'public105543780'})
 	result.append({'country':'Россия', 'city':'Красноярск', 'id_group':'public76867861'})
 	result.append({'country':'Россия', 'city':'Красноярск', 'id_group':'public80318218'})
+	
+	pass
+
+
+def AddPost():
+
+	pass
+
+def AddAddress():
 	
 	pass
