a312d3b1f4b1b87b6028c100ebe52e9cf8bdd72b
Денис Карасев <karasev.bpi1701@gmail.com>
2020-02-04 10:40:50 +0700

    Добавлено подключение к БД

--- lib/db_lib.py
+++ lib/db_lib.py
@@ -1,10 +1,30 @@
 """Либа для взаимодействия с базой данных"""
+import requests
+import uuid
+from datetime import datetime, timedelta
+import pymysql
+from contextlib import closing
+from pymysql.cursors import DictCursor
+import ast 
+import copy 
+
+connection = None
+
+def Connect(host='localhost', user='root', password='MnM32RtQt', db='dbo'):
+	global connection
+	connection = pymysql.connect(host=host, user=user, password=password, db=db, charset='utf8', cursorclass=DictCursor)
+def CloseConnect():
+	global connection
+	if connection != None: 
+		connection.close()
+
+
 
 def GetGroups():
 	"""Получение списка групп, по которым необходимо собирать информацию"""
 	result = list()
 	
-	result.append({'country':'Россия', 'city':'Красноярск', 'id_group':'public9751268'})
+	result.append({'country':'Россия', 'city':'Красноярск', 'id_group':'public9751268', 'url_group':'public9751268'})
 	result.append({'country':'Россия', 'city':'Красноярск', 'id_group':'public123114913'})
 	result.append({'country':'Россия', 'city':'Красноярск', 'id_group':'public105543780'})
 	result.append({'country':'Россия', 'city':'Красноярск', 'id_group':'public76867861'})
@@ -13,10 +33,22 @@ def GetGroups():
 	pass
 
 
-def AddPost():
+def AddPost(communityTitle='',):
 
+
+	# call test.sp_addPost(
+	# 	'йцу', # 
+	# 	'цу', 
+	# 	'цу', 
+	# 	йцу, 
+	# 	'йцу');
 	pass
 
 def AddAddress():
+	# call test.sp_addAddress('qwe', 'qwe', 'qwe', 'qwe', qwe, qwe);
 	
+	pass
+
+def AddTelephone():
+	# call test.sp_addTelephone('qwe', 'we');
 	pass
