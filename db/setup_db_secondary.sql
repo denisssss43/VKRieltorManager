1804b255e0cdea59a82a6c3b7b19e76c0e9f6a8d
Денис Карасев <karasev.bpi1701@gmail.com>
2020-02-04 10:15:29 +0700

    Исправлены комментарии

--- db/setup_db_secondary.sql
+++ db/setup_db_secondary.sql
@@ -1,30 +1,30 @@
--- Файл вторичной настройки БД и ее структуры
+/* Файл вторичной настройки БД и ее структуры */
 
-USE test; -- Указание субд работать с определенной БД
+USE test; # Указание субд работать с определенной БД
 
--- Создание хранимых процедур
+# Создание хранимых процедур
 DELIMITER $$
-CREATE PROCEDURE `sp_addPost`( -- Процедура добавления сырого (Без адреса) поста в БД 
-	`communityTitle` NVARCHAR(256), -- Ноименование сообщество в котором был найден пост
-	`description` NVARCHAR(512), -- Полное описание, указанное в посте (от редактированное)
-	`datetime` DATETIME,-- Дата публикации записи
-	`price` float, -- Цена предложения указанного в посте
-	`url` NVARCHAR(256)) -- URL-ссылка на пост
+CREATE PROCEDURE `sp_addPost`( /* Процедура добавления сырого (Без адреса) поста в БД */
+	`communityTitle` NVARCHAR(256), /* Ноименование сообщество в котором был найден пост */
+	`description` NVARCHAR(512), /* Полное описание, указанное в посте (от редактированное) */
+	`datetime` DATETIME, /* Дата публикации записи */
+	`price` float, /* Цена предложения указанного в посте */
+	`url` NVARCHAR(256)) /* URL-ссылка на пост */
 BEGIN
-	declare `_uuid_post` nvarchar(36); -- Переменная для хранения uuid поста
-	declare `_uuid_url` nvarchar(36); -- Переменная для хранения uuid url-ссылки
-	declare `_uuid_community` nvarchar(36); -- Переменная для хранения uuid сообщества
+	declare `_uuid_post` nvarchar(36); /* Переменная для хранения uuid поста */
+	declare `_uuid_url` nvarchar(36); /* Переменная для хранения uuid url-ссылки */
+	declare `_uuid_community` nvarchar(36); /* Переменная для хранения uuid сообщества */
 
-	set `_uuid_post` = ( -- Получение uuid для запиcи объявление
+	set `_uuid_post` = ( /* Получение uuid для запиcи объявление */
 		SELECT `post`.`uuid` 
 		FROM `post` 
 		WHERE `post`.`description` LIKE `description` 
 		OR `description` LIKE `post`.`description`
 		LIMIT 1);
 		
-	if `_uuid_post` IS NULL THEN -- Если запись не создана ранее
-		set `_uuid_post` = UUID(); -- Генерация нового uuid для поста
-		INSERT INTO `dbo`.`post` ( -- Добавление поста
+	if `_uuid_post` IS NULL THEN /* Если запись не создана ранее */
+		set `_uuid_post` = UUID(); /* Генерация нового uuid для поста */
+		INSERT INTO `dbo`.`post` ( /* Добавление поста */
 			`uuid`, 
 			`description`,
 			`price`, 
@@ -38,14 +38,14 @@ BEGIN
 			NULL);	
 	end if;
 
-	set `_uuid_community` = ( -- Получение uuid записи сообщества
+	set `_uuid_community` = ( /* Получение uuid записи сообщества */
 		SELECT `community`.`uuid` 
 		FROM `community` 
 		WHERE `community`.`title` = `communityTitle`);
 
-	if `_uuid_community` IS NULL THEN -- Если запись не создана ранее
-		set `_uuid_community` = UUID(); -- Генерация ного uuid для записи сообщества
-		INSERT INTO `community` ( -- Добавление записи сообщества 
+	if `_uuid_community` IS NULL THEN /* Если запись не создана ранее */
+		set `_uuid_community` = UUID(); /* Генерация ного uuid для записи сообщества */
+		INSERT INTO `community` ( /* Добавление записи сообщества */
 			`uuid`, 
 			`title`)
 		VALUES (
@@ -53,13 +53,13 @@ BEGIN
 			`communityTitle`);
 	end if;
 
-	set `_uuid_url` = ( -- Получение uuid записи url-адреса
+	set `_uuid_url` = ( /* Получение uuid записи url-адреса */
 		SELECT `link`.`uuid` 
 		FROM `link` 
 		WHERE `link`.`title` = `link`);
 
-	if `_uuid_url` IS NULL THEN -- Если запись не создана ранее
-		set `_uuid_url` = UUID(); -- Генерация uuid для новой записи url-адреса
+	if `_uuid_url` IS NULL THEN /* Если запись не создана ранее */
+		set `_uuid_url` = UUID(); /* Генерация uuid для новой записи url-адреса */
 		INSERT INTO `link` (
 			`uuid`, 
 			`url`,
@@ -77,21 +77,21 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE PROCEDURE `sp_addTelephone`( -- Процедура добавления телефонного номера 
-	`uuid_post` nvarchar(36), -- uuid поста к которому будет прикреплен номер телефона
-	`telephone` nvarchar(16)) -- номер телефона
+CREATE PROCEDURE `sp_addTelephone`( /* Процедура добавления телефонного номера */
+	`uuid_post` nvarchar(36), /* uuid поста к которому будет прикреплен номер телефона */
+	`telephone` nvarchar(16)) /* номер телефона */
 BEGIN
-	declare `_uuid_telephone` nvarchar(36); -- Переменная для хранения uuid телефона
-	declare `_is_telephone__post` TINYINT(1); -- Существует ли связь для текущего телефона и поста 
+	declare `_uuid_telephone` nvarchar(36); /* Переменная для хранения uuid телефона */
+	declare `_is_telephone__post` TINYINT(1); /* Существует ли связь для текущего телефона и поста */
 	
-	set _uuid_telephone = ( -- Получение uuid записи телефонного номера
+	set _uuid_telephone = ( /* Получение uuid записи телефонного номера */
 		SELECT `telephone`.`uuid` 
 		FROM `telephone` 
 		WHERE `telephone`.`number` LIKE telephone);
 	
-	IF _uuid_telephone IS NULL THEN	-- Если телефонный номер не был добавлен
-		set _uuid_telephone = UUID(); -- Генерация uuid для записи телефонного номера
-		INSERT INTO `telephone` ( -- Добавление записи телефонного номера
+	IF _uuid_telephone IS NULL THEN	/* Если телефонный номер не был добавлен */
+		set _uuid_telephone = UUID(); /* Генерация uuid для записи телефонного номера */
+		INSERT INTO `telephone` ( /* Добавление записи телефонного номера */
 			`uuid`, 
 			`number`) 
 		VALUES (
@@ -99,15 +99,15 @@ BEGIN
 			telephone);
 	end IF;	
 	
-	set `_is_telephone__post` = ( -- Проверка наличия свизи добавляемого телефона и объявления
+	set `_is_telephone__post` = ( /* Проверка наличия свизи добавляемого телефона и объявления */
 		SELECT COUNT(*) 
 		FROM `telephone__post` 
 		WHERE `telephone__post`.`uuid_telephone` LIKE _uuid_telephone
 		AND `telephone__post`.`uuid_post` LIKE _uuid_post
 		LIMIT 1);
 
-	IF _uuid_telephone__post = 0 THEN -- Если связь отсутствует
-		INSERT INTO `telephone__post` ( -- Добавление связи телефонного номера с постом
+	IF _uuid_telephone__post = 0 THEN /* Если связь отсутствует */
+		INSERT INTO `telephone__post` ( /* Добавление связи телефонного номера с постом */
 			`uuid_telephone`, 
 			`uuid_post`)
         VALUES (
@@ -119,26 +119,26 @@ DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE `sp_addAddress`(
-	`uuid_post` nvarchar(36), -- uuid поста к которому будет прикреплен номер телефона
-	`countryTitle` nvarchar(144), -- Наименование страны в которой актуален этот пост
-	`cityTitle` nvarchar(144), -- Наименование города в котором актуален этот пост 
-	`addressTitle` NVARCHAR(128), -- Географический адресс, указанный в посте
-	`latitude` FLOAT, -- Широта
-	`longitude` FLOAT) -- Долгота  
+	`uuid_post` nvarchar(36), /* uuid поста к которому будет прикреплен номер телефона */
+	`countryTitle` nvarchar(144), /* Наименование страны в которой актуален этот пост */
+	`cityTitle` nvarchar(144), /* Наименование города в котором актуален этот пост */
+	`addressTitle` NVARCHAR(128), /* Географический адресс, указанный в посте */
+	`latitude` FLOAT, /* Широта */
+	`longitude` FLOAT) /* Долгота */
 BEGIN
-	declare `_uuid_country` nvarchar(36); -- Переменная для хранения uuid страны
-	declare `_uuid_city` nvarchar(36); -- Переменная для хранения uuid города
-	declare `_uuid_address` nvarchar(36); -- Переменная для хранения uuid географического адреса
+	declare `_uuid_country` nvarchar(36); /* Переменная для хранения uuid страны */
+	declare `_uuid_city` nvarchar(36); /* Переменная для хранения uuid города */
+	declare `_uuid_address` nvarchar(36); /* Переменная для хранения uuid географического адреса */
 	
-	set `_uuid_country` = ( -- Получение uuid для запиcи города
+	set `_uuid_country` = ( /* Получение uuid для запиcи города */
 		SELECT `country`.`uuid` 
 		FROM `country`
 		WHERE `country`.`title` = `countryTitle`
 		LIMIT 1);
-	IF `_uuid_country` IS NULL THEN -- Если страна не была ранее добавлена
-		IF `countryTitle` IS NOT NULL THEN -- Если передано наименование страны
-			set `_uuid_country` = UUID(); -- Генерация нового uuid для записи города
-			INSERT INTO `country` ( -- Добавление записи города
+	IF `_uuid_country` IS NULL THEN /* Если страна не была ранее добавлена */
+		IF `countryTitle` IS NOT NULL THEN /* Если передано наименование страны */
+			set `_uuid_country` = UUID(); /* Генерация нового uuid для записи города */
+			INSERT INTO `country` ( /* Добавление записи города */
 				`uuid`, 
 				`title`)
 			VALUES (
@@ -147,15 +147,15 @@ BEGIN
 		end if;
 	end if;
 	    
-	set `_uuid_city` = ( -- Получение uuid для запиcи города
+	set `_uuid_city` = ( /* Получение uuid для запиcи города */
 		SELECT `city`.`uuid` 
 		FROM `city`
 		WHERE `city`.`title` = `cityTitle`
 		LIMIT 1);
-	IF `_uuid_city` IS NULL THEN -- Если город не был ранее добавлен
-		IF `cityTitle` IS NOT NULL THEN -- Если передано наименование города
-			set `_uuid_city` = UUID(); -- Генерация нового uuid для записи города
-			INSERT INTO `city` ( -- Добавление записи города
+	IF `_uuid_city` IS NULL THEN /* Если город не был ранее добавлен */
+		IF `cityTitle` IS NOT NULL THEN /* Если передано наименование города */
+			set `_uuid_city` = UUID(); /* Генерация нового uuid для записи города */
+			INSERT INTO `city` ( /* Добавление записи города */
 				`uuid`,
 				`uuid_country`, 
 				`title`)
@@ -166,14 +166,14 @@ BEGIN
 		end if;
 	end if;
 	
-	set `_uuid_address` = ( -- Получение uuid для запиcи адреcа
+	set `_uuid_address` = ( /* Получение uuid для запиcи адреcа */
 		SELECT `address`.`uuid` 
 		FROM `address` 
 		WHERE `address`.`title` = `address`
 		LIMIT 1);
-	IF `_uuid_address` IS NULL THEN -- Если адрес не был ранее добавлен
-		IF `address` IS NOT NULL THEN -- Если переданно текстовое представление адреса
-			set `_uuid_address` = UUID(); -- Генерация нового uuid для 
+	IF `_uuid_address` IS NULL THEN /* Если адрес не был ранее добавлен */
+		IF `address` IS NOT NULL THEN /* Если переданно текстовое представление адреса */
+			set `_uuid_address` = UUID(); /* Генерация нового uuid для */
 			INSERT INTO `address` (
 				`uuid`, 
 				`title`,
@@ -191,11 +191,11 @@ BEGIN
 END$$
 DELIMITER ;
 
--- Создание функций
+# Создание функций
 DELIMITER $$
-CREATE FUNCTION `f_address_uuidToText`( -- Функция конвертации uuid адреса в текстовое представление
-	`uuid_address` NVARCHAR(36)) -- Параметр uuid адреса
-	RETURNS NVARCHAR(128) -- Тип возвращаемых данных
+CREATE FUNCTION `f_address_uuidToText`( /* Функция конвертации uuid адреса в текстовое представление */
+	`uuid_address` NVARCHAR(36)) /* Параметр uuid адреса */
+	RETURNS NVARCHAR(128) /* Тип возвращаемых данных */
 BEGIN
 	RETURN (
 		SELECT `title` 
@@ -206,9 +206,9 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE FUNCTION `f_lastestPostDateTime`( -- Функция поиска последней даты для поста
-	`uuid_post` NVARCHAR(36)) -- Параметр uuid поста
-	RETURNS datetime -- Тип возвращаемых данных
+CREATE FUNCTION `f_lastestPostDateTime`( /* Функция поиска последней даты для поста */
+	`uuid_post` NVARCHAR(36)) /* Параметр uuid поста */
+	RETURNS datetime /* Тип возвращаемых данных */
 BEGIN
 	RETURN (
 		SELECT `datetime` 
@@ -220,9 +220,9 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE FUNCTION `f_lastestPostURL`( -- Функция поиска последней url-ссылки для поста
-	`uuid_post` NVARCHAR(36)) -- Параметр uuid поста
-	RETURNS NVARCHAR(256) -- Тип возвращаемых данных
+CREATE FUNCTION `f_lastestPostURL`( /* Функция поиска последней url-ссылки для поста */
+	`uuid_post` NVARCHAR(36)) /* Параметр uuid поста */
+	RETURNS NVARCHAR(256) /* Тип возвращаемых данных */
 BEGIN
 	RETURN (
 		SELECT `url` 
@@ -234,9 +234,9 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE FUNCTION `f_URLToPostUUID`( -- Функция поиска последней url-ссылки для поста
-	`url` NVARCHAR(256)) -- Параметр uuid поста
-	RETURNS NVARCHAR(36) -- Тип возвращаемых данных
+CREATE FUNCTION `f_URLToPostUUID`( /* Функция поиска последней url-ссылки для поста */
+	`url` NVARCHAR(256)) /* Параметр uuid поста */
+	RETURNS NVARCHAR(36) /* Тип возвращаемых данных */
 BEGIN
 	RETURN (
 		SELECT `uuid_post` 
@@ -247,18 +247,18 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE FUNCTION `f_distance_between_addresses`( -- Функция определения расстояния между двумя адресами
-	`uuid_address_from` NVARCHAR(36), -- Параметр uuid начального адреса
-	`uuid_address_to` NVARCHAR(36)) -- Параметр uuid конечного адреса
+CREATE FUNCTION `f_distance_between_addresses`( /* Функция определения расстояния между двумя адресами */
+	`uuid_address_from` NVARCHAR(36), /* Параметр uuid начального адреса */
+	`uuid_address_to` NVARCHAR(36)) /* Параметр uuid конечного адреса */
 	RETURNS FLOAT
 BEGIN
-	declare `w_from` FLOAT; -- Переменная для широты начального адреса
-	declare `v_from` FLOAT; -- Переменная для долготы начального адреса
+	declare `w_from` FLOAT; /* Переменная для широты начального адреса */
+	declare `v_from` FLOAT; /* Переменная для долготы начального адреса */
 	
-	declare `w_to` FLOAT; -- Переменная для широты конечного адреса
-	declare `v_to` FLOAT; -- Переменная для долготы конечного адреса
+	declare `w_to` FLOAT; /* Переменная для широты конечного адреса */
+	declare `v_to` FLOAT; /* Переменная для долготы конечного адреса */
 
-	-- Получение радиан 
+	/* Получение радиан */
 	set `w_from` = (
 		SELECT `address`.`latitude` 
 		FROM `address` 
@@ -280,7 +280,7 @@ BEGIN
 		WHERE `address`.`uuid` = `uuid_address_to` 
 		LIMIT 1) * PI() / 180;
 
-	-- Возврат расстояния
+	/* Возврат расстояния */
 	RETURN ACOS(SIN(`w_from`)*SIN(`w_to`) + COS(`w_from`)*COS(`w_to`)*COS(`w_to` - `v_to`));
 END$$
 DELIMITER ;
