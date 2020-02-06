9c2feb9c4ed35cfc181913ba0526e2f7a771c1ca
Денис Карасев <karasev.bpi1701@gmail.com>
2020-02-06 09:20:57 +0700

    Добавлен запуск поиска и анализа в главный скрипт

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 
 
+from lib.db_lib import AddPost, AddAddress, GetCommunity, GetCity, GetCountry, AddCommunity, CloseConnect, Connect
+from lib.parspost_lib import WallItemSearch, AddressFromDescription
 
 
+if __name__ == "__main__":
+	Connect()
+	
+	AddCommunity(countryTitle='Россия', cityTitle='Красноярск', communityURL='https://m.vk.com/public9751268')
+	AddCommunity(countryTitle='Россия', cityTitle='Красноярск', communityURL='https://m.vk.com/public123114913')
+	AddCommunity(countryTitle='Россия', cityTitle='Красноярск', communityURL='https://m.vk.com/public105543780')
+	AddCommunity(countryTitle='Россия', cityTitle='Красноярск', communityURL='https://m.vk.com/public76867861')
+	AddCommunity(countryTitle='Россия', cityTitle='Красноярск', communityURL='https://m.vk.com/public80318218')
+	
+	for i in range(18,500):
+		for community in GetCommunity():
 
-isRuningSearch = False
-def	RunSearch(): isRuningSearch = True
-def	StopSearch(): isRuningSearch = False
-def StartSearch():
+			city = GetCity(uuid=community['uuid_city']) 
+			country = GetCountry(uuid=city['uuid_country'])
 
-    pass
+			for wall_item in WallItemSearch(country=country['title'], city=city['title'], url_group=community['url'],offset=i*5):
+				print(i,end=': ')
+				post = AddPost(
+					wall_item['link_community'],
+					wall_item['description'], 
+					wall_item['date'], 
+					wall_item['price'], 
+					wall_item['link'], 
+					wall_item['telephones'])
+
+				if post['status'] == 2:
+					address = AddressFromDescription(
+						description=wall_item['description'], 
+						country=country['title'], 
+						city=city['title'])
+					AddAddress(
+						uuid_post=post['uuid'], 
+						countryTitle=country['title'], 
+						cityTitle=city['title'], 
+						addressTitle=address['address'], 
+						latitude=address['latitude'], 
+						longitude=address['longitude'])
+	CloseConnect()
