0cbcae1c8deadec249534cdfeab953c18fe4b639
Денис Карасев <karasev.bpi1701@gmail.com>
2020-03-12 12:32:02 +0700

    Исправление ошибок работы модуля конфигурации

--- vk_parser/lib/cfg/__init__.py
+++ vk_parser/lib/cfg/__init__.py
@@ -1,15 +1,20 @@
 """Либа взаимодействия с конфигурационным файлом"""
 
 import sqlite3
+import os
 
 connection = None # Объект подключения
 cursor = None # Объект курсора
+path = ""
 
 def Connect():
 	"""Подключение к конфигурационному файлу"""
 	global connection, cursor
 
-	connection = sqlite3.connect("vk_parser/lib/cfg/cfg.db")
+	# connection = sqlite3.connect(sys.path[0].replace('\\', '/')+'/cfg.db')
+
+	# print ('full path -', os.path.abspath(path+'cfg.db'))
+	connection = sqlite3.connect(os.path.abspath(path+'cfg.db'))
 	cursor = connection.cursor()
 	
 	sql = """ 
@@ -63,10 +68,9 @@ def SetParam(**kwargs):
 	Close()
 
 
-
-if __name__ == "__main__":
+def __set_param():
 	SetParam(
-		post_library_host=('localhost','значение хост библиотеки постов'), 
+		post_library_host=('172.16.17.67','значение хост библиотеки постов'), 
 		post_library_user=('usr_post_lib', 'имя пользователя с которого будет осуществлятся взаимодействие с библиотекой постов'), 
 		post_library_password=('MnM32RtQt', 'пароль пользователя с которого будет осуществляться взаимодействие с библиотекой постов'), 
 		post_library_db=('post_library', 'наименование библиотеки постов'),
@@ -78,7 +82,14 @@ if __name__ == "__main__":
 		
 		call_add_post_delay_ms=(60000, 'значение задержки запроса на добавление поста, мс'))
 
+if __name__ == "__main__":
 
+	path = "vk_parser/lib/cfg/"
+	__set_param()
+
+	path = "web-server/"
+	__set_param()
+	
 	Connect()
 	result = cursor.execute("SELECT * FROM param;").fetchall()
 	Close()
--- vk_parser/lib/cfg/cfg.py
+++ vk_parser/lib/cfg/cfg.py
@@ -4,22 +4,6 @@
 from . import GetParam
 
 
-def user_library_host():
-    """представление параметра user_library_host"""
-    return GetParam("user_library_host")
-
-def user_library_user():
-    """представление параметра user_library_user"""
-    return GetParam("user_library_user")
-
-def user_library_password():
-    """представление параметра user_library_password"""
-    return GetParam("user_library_password")
-
-def user_library_db():
-    """представление параметра user_library_db"""
-    return GetParam("user_library_db")
-
 def post_library_host():
     """представление параметра post_library_host - значение хост библиотеки постов"""
     return GetParam("post_library_host")
