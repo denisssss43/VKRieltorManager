9b9ab751780d3cdb34332a2ae0b090e58a543886
Денис Карасев <karasev.bpi1701@gmail.com>
2020-03-18 05:19:15 +0700

    Дополнение модуля работы с хранилищем постов

--- vk_parser/lib/db_post/__init__.py
+++ vk_parser/lib/db_post/__init__.py
@@ -1,24 +1,26 @@
-import uuid
+import uuid, pymysql, ast, copy
 from datetime import datetime, timedelta
-import pymysql
 from contextlib import closing
 from pymysql.cursors import DictCursor
-import ast 
-import copy
 
 from vk_parser.lib.cfg.cfg import Config
-
-connection = None # Объект подключения к бд 
 
 # NOTE:Изменено и откомментировано полностью
 def Connect():
 	"""Подключение к БД"""
-	global connection
-
 	cfg = Config()
 
 	try: 
-		connection = pymysql.connect(
+		print(
+			"""pymysql.connect(\n\thost={0},\n\tuser={1},\n\tpassword={2},\n\tdb={3},\n\tcharset={4},\n\tcursorclass=DictCursor,\n\tconnect_timeout={5})
+			""".format(
+				cfg.post_library_host, 
+				cfg.post_library_user, 
+				cfg.post_library_password, 
+				cfg.post_library_db, 
+				'utf8', 
+				.4))
+		return pymysql.connect(
 			host=cfg.post_library_host, 
 			user=cfg.post_library_user, 
 			password=cfg.post_library_password, 
@@ -27,7 +29,16 @@ def Connect():
 			cursorclass=DictCursor, 
 			connect_timeout=.4)
 	except: 
-		connection = pymysql.connect(
+		print(
+			"""pymysql.connect(\n\thost={0},\n\tuser={1},\n\tpassword={2},\n\tdb={3},\n\tcharset={4},\n\tcursorclass=DictCursor,\n\tconnect_timeout={5})
+			""".format(
+				'localhost', 
+				cfg.post_library_user, 
+				cfg.post_library_password, 
+				cfg.post_library_db, 
+				'utf8', 
+				.4))
+		return pymysql.connect(
 			host='localhost', 
 			user=cfg.post_library_user, 
 			password=cfg.post_library_password, 
@@ -35,39 +46,51 @@ def Connect():
 			charset='utf8', 
 			cursorclass=DictCursor, 
 			connect_timeout=.4)
+	finally:
+		print ('pymysql.connect None') 
+		return None
+
 # NOTE:Изменено и откомментировано полностью
-def CloseConnect():
+def CloseConnect(connection):
 	"""Закрытие подключения к БД"""
-	global connection
-	if connection != None: 
-		connection.close()
-		connection = None
+	if connection != None: connection.close()
 
 
-def GetCountry(uuid=''):
+def GetCountry(connection, uuid=''):
 	"""Получение страны по uuid (post_library)"""
-	result = None
+
 	if connection != None: # Если подключение создано
 		with connection.cursor() as cursor:
-			cursor.execute("SELECT * FROM post_library.country WHERE uuid='"+str(uuid)+"';")
-			result = cursor.fetchone()
-	return result
+			cursor.execute(
+				"""
+				SELECT * 
+				FROM post_library.country 
+				WHERE uuid='{0}';
+				""".format(str(uuid)))
+			return cursor.fetchone()
+	return None
 
-def GetCity(uuid=''):
+def GetCity(connection, uuid=''):
 	"""Получение города по uuid (post_library)"""
-	result = None
+	
 	if connection != None: # Если подключение создано
 		with connection.cursor() as cursor:
-			cursor.execute("SELECT * FROM post_library.city WHERE uuid='"+str(uuid)+"';")
-			result = cursor.fetchone()
-	return result
+			cursor.execute(
+				"""
+				SELECT * 
+				FROM post_library.city 
+				WHERE uuid='{0}';
+				""".format(str(uuid)))
+			return cursor.fetchone()
+	return None
 
-def GetCommunity():
+def GetCommunity(connection):
 	"""Получение списка групп, по которым необходимо собирать информацию (post_library)"""
-	result = None
+
 	if connection != None: # Если подключение создано
 		with connection.cursor() as cursor:
-			cursor.execute("""
+			cursor.execute(
+				"""
 				SELECT 
 					`id`, 
 					`uuid`, 
@@ -76,61 +99,96 @@ def GetCommunity():
 					`url` 
 				FROM post_library.community;
 				""")
-			result = cursor.fetchall()
-	return result
+			return cursor.fetchall()
+	return None
 
-def AddCommunity(countryTitle='', cityTitle='', communityURL=''):
+def AddCommunity(connection, countryTitle='', cityTitle='', communityURL=''):
 	"""Добавление сообщества в БД post_library"""
 
 	if connection != None: # Если подключение создано
 		with connection.cursor() as cursor:
-			cursor.execute("call post_library.sp_addCommunity('"+str(countryTitle)+"', '"+str(cityTitle)+"', '"+str(communityURL)+"');")
+			cursor.execute(
+				"""
+				call post_library.sp_addCommunity(
+					'{0}', 
+					'{1}', 
+					'{2}');
+				""".format(str(countryTitle),str(cityTitle),str(communityURL)))
 			connection.commit()
 	pass
 
-def AddPost(communityURL='', description='', dateTime=datetime.now(), price=0.0, url='', telephones=[]):
+def AddPost(connection, communityURL='', description='', dateTime=datetime.now(), price=0.0, url='', telephones=[]):
 	"""Добавление поста в БД (post_library)"""
-	result = {'uuid':'', 'status':0}
+	result = {'uuid':None, 'status':None}
 	
 	if price <= 0: return result
 
 	if connection != None: # Если подключение создано
 		with connection.cursor() as cursor:
-			cursor.execute("call post_library.sp_addPost('"+str(communityURL)+"','"+str(description)[:1024]+"','"+str(dateTime)+"',"+str(price)+",'"+str(url)+"');")
+			cursor.execute(
+				"""
+				call post_library.sp_addPost(
+					'{0}',
+					'{1}',
+					'{2}',
+					{3},
+					'{4}');
+				""".format(communityURL, description[:1024], str(dateTime), str(price), url))
 			result = cursor.fetchone()
 			for num in telephones:
-				AddTelephone(result['uuid'], str(num))
+				AddTelephone(
+					connection, 
+					result['uuid'], 
+					str(num))
 			connection.commit()
-
 	return result
 
-def AddTelephone(uuid_post='',telephone=''):
+def AddTelephone(connection, uuid_post='',telephone=''):
 	"""Добавление телефона в БД (post_library)"""
 
 	if connection != None: # Если подключение создано
 		with connection.cursor() as cursor:
-			cursor.execute("call post_library.sp_addTelephone('"+str(uuid_post)+"', '"+str(telephone)+"');")
+			cursor.execute(
+				"""
+				call post_library.sp_addTelephone(
+					'{0}', 
+					'{1}');
+				""".format(uuid_post, telephone))
 			connection.commit()
 	pass
 
-def AddAddress(uuid_post='', countryTitle='', cityTitle='', addressTitle='', latitude=0.0, longitude=0.0):
+def AddAddress(connection, uuid_post='', countryTitle='', cityTitle='', addressTitle='', latitude=0.0, longitude=0.0):
 	"""Добавление адреса в БД (post_library)"""
 
 	if connection != None: # Если подключение создано
 		addressTitle = str(addressTitle) if addressTitle=='NULL' else ("'"+str(addressTitle)+"'")
 		with connection.cursor() as cursor:
-			cursor.execute("call post_library.sp_addAddress('"+str(uuid_post)+"', '"+str(countryTitle)+"', '"+str(cityTitle)+"', " + addressTitle + ", "+str(latitude)+", "+str(longitude)+");")
+			cursor.execute(
+				"""
+				call post_library.sp_addAddress(
+					'{0}', 
+					'{1}', 
+					'{2}', 
+					{3}, 
+					{4}, 
+					{5});
+				""".format(uuid_post, countryTitle, cityTitle, addressTitle, str(latitude), str(longitude)))
 			connection.commit()
-	
 	pass
 
-def AddImg(uuid_post='', _img_url=''):
+def AddImg(connection, uuid_post='', _img_url=''):
 	"""Добавление изображения в БД (post_library)"""
 	print ("uuid post:{0} img url:{1}".format(uuid_post,_img_url))
 	if connection != None: # Если подключение создано
 		with connection.cursor() as cursor:
-			cursor.execute("call post_library.sp_addImg('"+str(uuid_post)+"', '"+str(_img_url)+"');")
+			cursor.execute(
+				"""
+				call post_library.sp_addImg(
+					'{0}', 
+					'{1}');
+				""".format(uuid_post, _img_url))
 			connection.commit()
+	pass
 
 
 
