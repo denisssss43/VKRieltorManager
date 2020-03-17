6229c0f2a0ed8e191832cbdc1faff4261ed609e8
Denis <38821830+denisssss43@users.noreply.github.com>
2020-03-17 09:21:51 +0700

    вывод конфигуратора в объект

--- vk_parser/lib/cfg/cfg.py
+++ vk_parser/lib/cfg/cfg.py
@@ -3,24 +3,22 @@
 
 from . import GetParam
 
+class Config(object):
+	""" Объект-конфигуратор """
 
-def post_library_host():
-    """представление параметра post_library_host - значение хост библиотеки постов"""
-    return GetParam("post_library_host")
+	# Представление параметра post_library_host - значение хост библиотеки постов
+	post_library_host = GetParam("post_library_host")
 
-def post_library_user():
-    """представление параметра post_library_user - имя пользователя с которого будет осуществлятся взаимодействие с библиотекой постов"""
-    return GetParam("post_library_user")
+	# Представление параметра post_library_user - имя пользователя с которого будет осуществлятся взаимодействие с библиотекой постов
+	post_library_user = GetParam("post_library_user")
 
-def post_library_password():
-    """представление параметра post_library_password - пароль пользователя с которого будет осуществляться взаимодействие с библиотекой постов"""
-    return GetParam("post_library_password")
+	# Представление параметра post_library_password - пароль пользователя с которого будет осуществляться взаимодействие с библиотекой постов
+	post_library_password = GetParam("post_library_password")
 
-def post_library_db():
-    """представление параметра post_library_db - наименование библиотеки постов"""
-    return GetParam("post_library_db")
+	# Представление параметра post_library_db - наименование библиотеки постов
+	post_library_db = GetParam("post_library_db")
 
-def call_add_post_delay_ms():
-    """представление параметра call_add_post_delay_ms - значение задержки запроса на добавление поста, мс"""
-    return GetParam("call_add_post_delay_ms")
+	# Представление параметра call_add_post_delay_ms - значение задержки запроса на добавление поста, мс
+	call_add_post_delay_ms = GetParam("call_add_post_delay_ms")
 
+	pass
