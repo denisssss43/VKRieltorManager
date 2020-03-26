bfc997b634d8567777289a7501253bb8fe27ce2b
Denis <38821830+denisssss43@users.noreply.github.com>
2020-03-27 06:16:11 +0700

    Добавление библиотек

--- setup.py
+++ setup.py
@@ -9,31 +9,33 @@ echo Установка виртуальной среды:
 echo install venv VKRieltorManager
 cd .. & py -m venv VKRieltorManager
 
+cd {2}
+
 echo Запуск виртуальной среды ({0}/activate): 
-{0}/activate
+{0}/activate.bat
 
 echo Обновление pip и установка/обновление всех необходимых библиотек:
 echo upgrade pip
 python -m pip install --upgrade pip
-
 echo install Flask
-echo # pip install Flask
+pip install Flask
+echo install db-sqlite3
+pip install db-sqlite3
+echo install PyMySQL
+pip install PyMySQL
+
+echo Создание конфиг-файла:
+py server\\_lib\cfg\\setup_cfg.py
 
 echo Вывод установленных библиотек виртуальной среды:
-echo # pip list
-
-echo Остановка виртуальной машины
-deactive
-
-echo Строки с пометкой '#' были пропущены.
+pip list
 
 """.format(
-		os.path.abspath('Scripts/'), 
-		os.path.abspath('web_server/'), 
+		os.path.abspath('Scripts'), 
+		os.path.abspath('web_server'), 
 		os.path.abspath('')
 	).replace('\\','/').replace('\n\n', '& echo --- & ').replace('\n', ' & ')
 
 
 if __name__ == "__main__":
-	print ('\nsetup_start run abs_PATH {0} \n\n'.format(os.path.abspath('')))
 	os.system(cmd)
